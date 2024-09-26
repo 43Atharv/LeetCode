aa9894f3b6a75f4dd43dec4ccbe9d9e894f413d6
Atharv Chougule <154349297+43Atharv@users.noreply.github.com>
2024-09-26 18:53:45 +0530

    Time: 14 ms (51.77%), Space: 11.6 MB (85.28%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        lower_case_str = ""
+        for char in s:
+            ascii_val = ord(char)
+            if 65 <= ascii_val <= 90:  # A-Z in ASCII
+                lower_case_str += chr(ascii_val + 32)
+            else:
+                lower_case_str += char
+        return lower_case_str
